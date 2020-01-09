73c047c2725a8f35cc9e6273279c59b0b7644343
BeauvR <beauvanrouwendal@gmail.com>
2020-01-09 15:16:07 +0100

    uitwerking

---
+++ script.js
@@ -0,0 +1,15 @@
+var obj = {
+    Naam: "Zara",
+    Score: 36,
+    Begin_datum: "21-01-2016",
+    Vrienden: [
+        "Mika",
+        "Josef",
+        "Maria",
+        "Sumail",
+        "Lotte"
+    ],
+    Gekwalificeerd: true
+};
+
+console.log(obj);
